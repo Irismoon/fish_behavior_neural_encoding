77f4ff9002fae0305637e0b43b3bcbd6687a9be6
Irismoon <1121490236@qq.com>
2020-11-05 09:38:04 +0800

    select bouts with more criteria

--- behavior/select_bouts.m
+++ behavior/select_bouts.m
@@ -3,7 +3,7 @@ function [outputArg1,outputArg2] = select_bouts(inputArg1,inputArg2)
 numfish = length(sessionID);
 for iFish=1:numfish
     load(fullfile(getpath('behavior',sessionID{iFish},fishID{iFish}),'high_analysis'),'conv_or_not');
-    load(fullfile(getpath('behavior',sessionID{iFish},fishID{iFish}),'tail_swing'),'sum_curv','curvdata','bout_idx','new_centerline_20');
+    load(fullfile(getpath('behavior',sessionID{iFish},fishID{iFish}),'tail_swing'),'angledata','sum_curv','curvdata','bout_idx','new_centerline_20');
     load(fullfile(getpath('behavior',sessionID{iFish},fishID{iFish}),'low_video_analysis_result'),'param_head_angle_all','param_head_dist_all','no_param');
     if (length(conv_or_not)-length(curvdata))>-5 && (length(conv_or_not)-length(curvdata))<=0
         conv_or_not = [conv_or_not;zeros(length(curvdata)-length(conv_or_not),1)];
@@ -12,18 +12,19 @@ for iFish=1:numfish
     else
         load(fullfile(getpath('behavior',sessionID{iFish},fishID{iFish}),'align_with_fluo'));
         conv_or_not = conv_or_not(align_with_fluo_high==1);
-        [curvdata,new_centerline_20,param_head_angle_all,param_head_dist_all,no_param] = samfnmultvar(@(x) x(align_with_fluo_low==1,:,:),curvdata,new_centerline_20,...
+        [angledata,sum_curv,curvdata,new_centerline_20,param_head_angle_all,param_head_dist_all,no_param] = samfnmultvar(@(x) x(align_with_fluo_low==1,:,:),...
+            angledata,sum_curv,curvdata,new_centerline_20,...
             param_head_angle_all,param_head_dist_all,no_param);
     end
     %only select bouts with eye converge
-    conv_mask = arrayfun(@(i) mean(conv_or_not(bout_idx(i,1):bout_idx(i,2))),1:size(bout_idx,1))>0;%
+    conv_mask = arrayfun(@(i) mean(conv_or_not(bout_idx(i,1):bout_idx(i,2))),1:size(bout_idx,1))>0.5;%
     bout_idx = bout_idx(conv_mask,:);
     %only select bouts with prey in the visual field
     prey_mask = arrayfun(@(i) mean(no_param(bout_idx(i,1):bout_idx(i,2))),1:size(bout_idx,1))>0;%
     bout_idx = bout_idx(prey_mask,:);
     %only select bouts responding correctly to the prey when prey is within
     %the visual field
-    startFrame = bout_idx(:,1);
+    startFrame = bout_idx(:,1);endFrame = bout_idx(:,2);
     param_head_angle_move = zeros(length(startFrame),1);param_head_dist_move = param_head_angle_move;
     for iframe=1:length(startFrame)
         if startFrame(iframe)>20
@@ -35,19 +36,38 @@ for iFish=1:numfish
                 param_head_angle_all,param_head_dist_all);
         end
     end
-    [sign_curv,sum_curv_move] = arrayfun(@(i) maxabsk(sum_curv(startFrame(i):startFrame(i)+5),2),1:length(startFrame));
-    %we keep forward and corect turns and delete those obviously wrong
-    %turns, 
-%     [bout_idx,sum_curv_move,param_head_angle_move] = samfnmultvar(@(x) x(sign_curv>0,:),bout_idx,row2col(sum_curv_move,1),row2col(param_head_angle_move));
-    %angle<0 when at right, angle>0 when at left, curv<0 towards right
-    %while curv>0 towards left
-    idx = (row2col(sum_curv_move,1) .* row2col(param_head_angle_move,1))>0;
-    good_bout_idx = bout_idx(idx | (row2col(sign_curv,1)<0),:);%same direction between prey and tail swing, or forward
+    %first feature of J-turn
+    %consistent swing direction and prey location
+    [sign_curv,absmax_sum_curv_move] = arrayfun(@(i) maxabsk(sum_curv(startFrame(i):endFrame(i)),3),1:length(startFrame));
+    idx_J = (row2col(absmax_sum_curv_move,1) .* row2col(param_head_angle_move,1))>0 & row2col(sign_curv,1);%same direction with prey and continuous same direction swing
+    %2nd feature of J-turn
+    %no very large opposite directional swing 
+    min_sum_curv_move = arrayfun(@(i) min(sum_curv(startFrame(i):endFrame(i))),1:length(startFrame));
+    max_sum_curv_move = arrayfun(@(i) max(sum_curv(startFrame(i):endFrame(i))),1:length(startFrame));
+    for iFrame=1:length(startFrame)
+    if sign(absmax_sum_curv_move(iFrame))>0&&sign(min_sum_curv_move(iFrame))<0
+        idx_J(iFrame) = idx_J(iFrame)&(abs(min_sum_curv_move(iFrame))<0.8);
+    elseif sign(absmax_sum_curv_move(iFrame))<0&&sign(max_sum_curv_move(iFrame))>0
+        idx_J(iFrame) = idx_J(iFrame)&(max_sum_curv_move(iFrame)<0.8);
+    end
+    end
+    %3rd feature of J-turn
+    %no very large left->right/right->left beat between continuous frame
+    for iFrame=1:length(startFrame)
+        tmp = sum_curv(startFrame(iFrame):endFrame(iFrame));
+        tmp = [tmp(1:end-1) tmp(2:end)];
+        idx_J(iFrame) = idx_J(iFrame)&(max(abs(tmp(:,2) - tmp(:,1)))<1.5);
+    end
+    %4th feature of J-turn %ToDo
+    %left and right turn angle not too large
+%     idx2 = swing_angle<45;
+    %1st feature of forward.
+    %left/right small-angle beat
+    swing_angle = rad2deg(arrayfun(@(i) max(angledata(startFrame(i):endFrame(i),end)-angledata(startFrame(i):endFrame(i),1)),1:length(startFrame)));
+    
+    idx_forward = (row2col(sign_curv,1)==0&row2col(swing_angle,1)<20);%
+    good_bout_idx = bout_idx(idx_J | idx_forward,:);%same direction between prey and tail swing, or forward
     save(fullfile(getpath('behavior',sessionID{iFish},fishID{iFish}),'tail_swing'),'good_bout_idx','-append');
 end
 end
-function [y,yy] = maxabsk(x,k)
-[~,I] = maxk(abs(x),k);
-y = prod(x(I));
-yy = x(I(1));
-end
+
